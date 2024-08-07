2cbe17af630b15e3348488285f7e94494bbcc89d
keronasha <knashat47@gmail.com>
2024-08-07 12:38:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let imagg = document.querySelector('.imagg')
+let conteiner = document.querySelector('.conteiner')
+function phones(phone){
+    imagg.src = phone;
+}
+ 
+function colors(color){
+    conteiner.style.background = color;
+}
